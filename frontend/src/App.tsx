619336eebeaca4d136f201d2aa8a944106eaaefd
alypse <44419772+alypse@users.noreply.github.com>
2024-06-08 12:04:22 +0100

    Repo structure with fe and be

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+// import logo from '/logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Here is the react app!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
